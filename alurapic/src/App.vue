f810f97795ad646d286153b470a950220dc0c073
Breno Macena Pereira de Souza <breno@i3t.com.br>
2021-03-09 21:21:12 -0300

    .

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -1,13 +1,28 @@
 <template>
   <div class="corpo">
-    <router-view></router-view>
+    
+    <meu-menu :rotas="routes" />
+
+    <transition name="pagina">
+      <router-view></router-view>
+    </transition>
+
   </div>
 </template>
 
 <script>
+import { routes } from "./routes";
+import Menu from './components/shared/menu/Menu.vue';
 
 export default {
-
+  components: {
+    'meu-menu': Menu
+  },
+  data() {
+    return {
+      routes
+    };
+  }
 };
 </script>
 
@@ -16,5 +31,15 @@ export default {
   font-family: Helvetica;
   width: 96%;
   margin: 0 auto;
+}
+
+.pagina-enter,
+.pagina-leave-active {
+  opacity: 0;
+}
+
+.pagina-enter-active,
+.pagina-leave-active {
+  transition: opacity 0.4s;
 }
 </style>
---
+++ alurapic/src/components/shared/menu/Menu.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav>
+    <ul>
+      <li v-for="rota in rotas" :key="rota.title">
+        <router-link :to="rota.path ? rota.path : '/'">
+          {{ rota.title }}
+        </router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+<script>
+export default {
+  props: {
+      rotas: {
+          type: Array,
+          required: true
+      }
+  }
+};
+</script>
+<style scoped></style>
